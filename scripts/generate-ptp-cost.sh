d41640d974be56478efce1535cd1771af481cc2b
Soha Jin <soha@lohu.info>
2021-02-19 13:01:20 +0800

    update

--- scripts/generate-ptp-cost.sh
+++ scripts/generate-ptp-cost.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-for with_ll in $(cat /proc/net/if_inet6 | grep "^fe80" | tr -s ' ' | cut -d ' ' -f 6)
-do
-	if [ $(expr \( $(($(cat /sys/class/net/$with_ll/flags))) / 16 \) % 2) -ne 1 ]
-	then
+for with_ll in $(cat /proc/net/if_inet6 | grep "^fe80" | tr -s ' ' | cut -d ' ' -f 6); do
+	if [ $(expr \( $(($(cat /sys/class/net/$with_ll/flags))) / 16 \) % 2) -ne 1 ]; then
 		continue
 	fi
 	cost=65535
 	ping_result=$(ping -q -L -n -i 1 -c 10 -W 10 ff02::1%$with_ll)
 	ping_loss=$(echo "$ping_result" | grep -oP '\d+(?=% packet loss)')
-	if [ $ping_loss -ne 100 ]
-	then
+	if [ $ping_loss -ne 100 ]; then
 		ping_rtt=$(echo "$ping_result" | tail -1 | cut -d ' ' -f 4 | cut -d '/' -f 2)
 		cost=$(echo | awk '{ printf "%0.0f\n", ('$ping_rtt' * 100 / (100 - '$ping_loss') * 10); }')
 	fi
